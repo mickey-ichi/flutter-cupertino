ce0fa26890c804b585a9228451eba9e8d9b02192
mickey <mickey@react-plus.com>
2021-04-08 15:53:07 +0700

    init

---
+++ lib/screens/home/HomeScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Cupertino Store'),
+      ),
+      child: SizedBox(),
+    );
+  }
+}
